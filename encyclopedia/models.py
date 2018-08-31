c0038ae144d70f21749c3b4f92b1f6e3c6fa93d5
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-08-31 23:24:19 +0500

    Created necessary models for Question, Category and Answers

--- encyclopedia/models.py
+++ encyclopedia/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    """ Category model for questions"""
+    name    = models.CharField(blank=False, max_length=50)
+    
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Question(models.Model):
+    """ Model for the questions in encyclopedia"""
+
+    user     = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    category = models.ManyToManyField(to=Category)
+    question = models.CharField(blank=False, max_length=250)
+    
+
+
+    def __str__(self):
+        return self.question
+
+
+
+
+
+class Answer(models.Model):
+    """ Model for answers of Questions """
+    user      = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    questions = models.ForeignKey(to=Question, on_delete=models.CASCADE)
+    text      = models.CharField(blank=False, max_length=50)
+    value     = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.text
