8ce5d571ae8657b1d0d40a43b6f48ac8ff6ecd44
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-04 03:59:19 +0500

    Changed the admin layout for question and added answers as tabular inline

--- encyclopedia/admin.py
+++ encyclopedia/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Question, Category, Answer
 # Register your models here.
-admin.site.register(Question)
+# admin.site.register(Question)
 admin.site.register(Category)
 admin.site.register(Answer)
+
+
+class AnswerInline(admin.TabularInline):
+    '''Tabular Inline View for Category'''
+
+    model = Answer
+    min_num = 1
+    max_num = 5
+
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    """ The Admin view of the Question and related models """
+
+
+    inlines = [AnswerInline]
+    
