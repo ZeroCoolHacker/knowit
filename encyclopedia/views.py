bb47301fa7908f3e45261dc8b5f336b0259bb5ac
ZeroCoolHacker <shahzaibakash@gmail.com>
2018-09-15 19:53:07 +0500

    Created the index view for encyclopedia app

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Question
+
 
 # Create your views here.
+def index_view(request):
+    qs = Question.objects.all()
+    return render(request, 'encyclopedia/index.html', {'qs': qs})
